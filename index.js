6c92826f8b7cbac567ada3c17bee22b8b37cd13e
eric mutuma <ericmutuma15@gmail.com>
2024-03-23 23:05:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "user"
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}, !`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK,  ${upperCaseCurrentUser}, !`
+const shortGreeting = `Welcome, ${currentUser[0]}, !`
